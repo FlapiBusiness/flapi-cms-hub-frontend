198ff08cf5bf2e70175c41acc6748b20fc4afd5e
greglmx0 <gregoire.lamoureux@epitech.eu>
2024-12-15 22:41:51 +0100

    chore: add custom font sizes to Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,15 @@ import type { Config } from 'tailwindcss'
 export default <Partial<Config>>{
   content: ['./src-nuxt/pages/**/*.vue', './src-nuxt/components/**/*.vue', './src-nuxt/layouts/**/*.vue', './app.vue'],
   theme: {
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
     screens: {
       xs: '375px',
       sm: '640px',
