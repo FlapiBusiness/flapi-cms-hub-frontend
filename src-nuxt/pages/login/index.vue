b4f785d092f381648cffc0003b27c4fd3c5df8f2
greglmx0 <gregoire.lamoureux@epitech.eu>
2024-12-15 21:45:24 +0100

    fix: update signup link in login page to use relative path

--- src-nuxt/pages/login/index.vue
+++ src-nuxt/pages/login/index.vue
@@ -5,7 +5,7 @@
       <LoginForm />
       <h6 class="flex h-full flex-wrap items-center justify-center gap-2 text-sm font-medium text-light-400">
         Vous n'avez pas de compte ?
-        <flapiLink class="text-primary-light text-sm" link="https://signup"> S'inscrire</flapiLink>
+        <flapiLink class="text-primary-light text-sm" link="signup"> S'inscrire</flapiLink>
       </h6>
     </div>
   </div>
