9dc7b9e419b5d955ada57dce56a5b90d82839fd8
UDeficit <gregoire.lamoureux@epitech.eu>
2024-12-15 04:49:48 +0100

    feat: add loading screen component with FlapiSpinner

---
+++ src-nuxt/pages/loading-screen/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex min-h-screen items-center justify-center bg-grey-700">
+    <FlapiSpinner variant="light" :size="64" />
+  </div>
+</template>
+<script lang="ts" setup>
+import FlapiSpinner from '~/components/common/ui/flapiSpinner.vue'
+</script>
