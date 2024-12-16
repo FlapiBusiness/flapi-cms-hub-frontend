22e09b647e2c43f818ca5da5adab27f797e96370
greglmx0 <gregoire.lamoureux@epitech.eu>
2024-12-17 00:47:05 +0100

    feat: add headings for application photo and customization sections in setup app page

--- src-nuxt/pages/setup/app/index.vue
+++ src-nuxt/pages/setup/app/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex min-h-screen flex-col items-center justify-center bg-gray-700">
     <div class="grid w-full max-w-7xl gap-4">
+      <p class="text-2xl font-semibold text-light-400">Photo de l'applications</p>
       <FlapiCardDownloadPicture picture="https://picsum.photos/136/136" />
+      <p class="text-2xl font-semibold text-light-400">Personnalisation de l'application</p>
       <FlapiFormCard> <div class="h-96"></div> </FlapiFormCard>
     </div>
   </div>
