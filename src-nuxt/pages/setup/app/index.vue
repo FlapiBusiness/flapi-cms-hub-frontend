15c39e20ddc3310bd582e85987926cfd290d67ea
greglmx0 <gregoire.lamoureux@epitech.eu>
2024-12-17 00:33:27 +0100

    feat: create setup app page with FlapiFormCard and FlapiCardDownloadPicture components

---
+++ src-nuxt/pages/setup/app/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex min-h-screen flex-col items-center justify-center bg-gray-700">
+    <div class="grid w-full max-w-7xl gap-4">
+      <FlapiCardDownloadPicture picture="https://picsum.photos/136/136" />
+      <FlapiFormCard> <div class="h-96"></div> </FlapiFormCard>
+    </div>
+  </div>
+</template>
+<script lang="ts"></script>
+<script lang="ts" setup>
+import FlapiFormCard from '@/components/cards/FlapiFormCard.vue'
+import FlapiCardDownloadPicture from '@/components/cards/FlapiCardDownloadPicture.vue'
+</script>
