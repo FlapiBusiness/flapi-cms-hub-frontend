355797242f4229393ccb95fff2795d7fedefd9d2
greglmx0 <gregoire.lamoureux@epitech.eu>
2024-12-17 00:48:07 +0100

    feat: create settings app page with photo and customization sections

---
+++ src-nuxt/pages/settings/app/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex min-h-screen flex-col items-center justify-center bg-gray-700">
+    <div class="grid w-full max-w-7xl gap-4">
+      <p class="text-2xl font-semibold text-light-400">Photo de l'applications</p>
+      <FlapiCardDownloadPicture picture="https://picsum.photos/136/136" />
+      <p class="text-2xl font-semibold text-light-400">Personnalisation de l'application</p>
+      <FlapiFormCard> <div class="h-96"></div> </FlapiFormCard>
+    </div>
+  </div>
+</template>
+<script lang="ts"></script>
+<script lang="ts" setup>
+import FlapiFormCard from '@/components/cards/FlapiFormCard.vue'
+import FlapiCardDownloadPicture from '@/components/cards/FlapiCardDownloadPicture.vue'
+</script>
