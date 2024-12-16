fc65b57912a9fd6b6f2529509dc8466bada596ac
greglmx0 <gregoire.lamoureux@epitech.eu>
2024-12-16 23:47:17 +0100

    feat: add FlapiCardDownloadPicture component for image upload functionality

---
+++ src-nuxt/components/cards/FlapiCardDownloadPicture.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex flex-row rounded-lg border border-gray-300 bg-gray-500 p-4">
+    <img :src="props.picture" alt="Card picture" class="h-34 w-34 rounded-lg" />
+    <div class="ml-8 flex flex-col items-start justify-center">
+      <FlapiButton size="md" backgroundColor="#35424D" @click="addPicture"> Ajouter une image </FlapiButton>
+      <p class="mt-6 text-base font-semibold text-light-400">
+        L’image doit être au format JPEG, PNG ou GIF et ne doit pas dépasser 10 Mo.
+      </p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+/**
+ * Type definitions for the Card component props
+ * @type {CardProps}
+ * @property {string} picture - The picture of the card
+ */
+export type CardProps = {
+  picture: string
+}
+</script>
+
+<script lang="ts" setup>
+import { defineProps } from '@vue/runtime-core'
+import FlapiButton from '@/components/common/core/FlapiButton.vue'
+
+/**
+ * Props
+ * @param {string} picture - The picture of the card
+ */
+const props: CardProps = defineProps({
+  picture: {
+    type: String,
+    required: true,
+  },
+})
+
+/**
+ * Add picture
+ */
+const addPicture: () => void = () => {
+  console.log('Add picture')
+}
+</script>
