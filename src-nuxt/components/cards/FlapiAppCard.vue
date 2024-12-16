04f6c3aac8d4e3423e49d05c57c7cf0e13752f49
greglmx0 <gregoire.lamoureux@epitech.eu>
2024-12-16 22:44:50 +0100

    feat: add FlapiAppCard component and application store; implement applications page

---
+++ src-nuxt/components/cards/FlapiAppCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex w-full max-w-60 flex-col items-start justify-center rounded-lg bg-gray-300 p-4">
+    <div class="flex flex-col">
+      <img :src="props.picture" alt="Card picture" class="h-30 w-30 rounded-lg" />
+      <p class="mt-4 text-base font-semibold text-light-400">{{ props.title }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+/**
+ * Type definitions for the Card component props
+ * @type {CardProps}
+ * @property {string} title - The title of the card
+ * @property {string} picture - The picture of the card
+ */
+export type CardProps = {
+  title: string
+  picture: string
+}
+</script>
+
+<script lang="ts" setup>
+import { defineProps } from '@vue/runtime-core'
+
+/**
+ * Props
+ * @param {string} title - The title of the card
+ * @param {string} picture - The picture of the card
+ */
+const props: CardProps = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  picture: {
+    type: String,
+    required: true,
+  },
+})
+</script>
---
+++ src-nuxt/pages/apps/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex min-h-screen flex-col items-center justify-center bg-gray-700">
+    <p class="mb-8 text-center text-4xl text-light-400">Choisir votre application</p>
+    <FlapiButton type="submit">
+      <p class="font-semibold text-light-400">Nouvelle application</p>
+    </FlapiButton>
+    <div
+      class="mt-8 grid w-full max-w-5xl grid-cols-1 justify-items-center gap-4 px-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+    >
+      <FlapiAppCard v-for="app in applications" :key="app.id" :title="app.title" :picture="app.picture" />
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import FlapiButton from '@/components/common/core/FlapiButton.vue'
+import { useApplicationStore } from '~/stores/applicationStore'
+import type { Application } from '~/stores/applicationStore'
+import FlapiAppCard from '~/components/cards/FlapiAppCard.vue'
+
+const applicationStore: ReturnType<typeof useApplicationStore> = useApplicationStore()
+await applicationStore.fetchApplications()
+const applications: Application[] = applicationStore.applications
+
+// const applications = applicationStore.applications
+</script>
