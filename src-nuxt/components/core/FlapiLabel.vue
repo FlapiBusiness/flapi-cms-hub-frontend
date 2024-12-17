ae6bc96fe0fcd92a1625ec744f3d0887c643c052
Gregoire Lamoureux <gregoire.lamoureux@eiffage.com>
2024-12-17 13:52:59 +0100

    feat: add FlapiLabel and FlapiSelect components for form handling; update search icon

---
+++ src-nuxt/components/core/FlapiLabel.vue
@@ -0,0 +1,32 @@
+<template>
+  <label :for="props.id" class="text-base font-medium text-light-400">
+    <slot />
+  </label>
+</template>
+
+<script lang="ts">
+/**
+ * Type definitions for the FlapiLabel component props
+ * @type {FlapiLabelProps}
+ * @property {string} id - The id of the label
+ */
+export type FlapiLabelProps = {
+  id: string
+}
+</script>
+
+<script lang="ts" setup>
+import { defineProps } from '@vue/runtime-core'
+
+/**
+ * Type definitions for the FlapiLabel component props
+ * @type {FlapiLabelProps}
+ * @property {string} id - The id of the label
+ */
+const props: FlapiLabelProps = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+})
+</script>
--- src-nuxt/components/icons/search.vue
+++ src-nuxt/components/icons/search.vue
@@ -1,4 +1,23 @@
-<template>
+<!-- <template>
   <circle cx="11" cy="11" r="8" />
   <line x1="21" y1="21" x2="16.65" y2="16.65" />
+</template> -->
+
+<template>
+  <svg fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z"
+      stroke="#F5F4FB"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M17.5 17.5L13.875 13.875"
+      stroke="#F5F4FB"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
 </template>
