773589544d1f5e27a9b20a011093435522acccd0
Léo Guillaume <leo.guillaume@epitech.eu>
2024-12-19 11:13:25 +0100

    feat: page links and create app form

--- src-nuxt/components/forms/CreateApplicationForm.vue
+++ src-nuxt/components/forms/CreateApplicationForm.vue
@@ -1,36 +1,58 @@
 <template>
   <div class="grid grid-cols-2 gap-3 p-4">
-    <FlapiInput v-model:value="payload.customerName" label="Nom du client" placeholder="John Doe" required />
     <FlapiInput
-      v-model:value="payload.projectName"
+      :value="props.payload.customerName"
+      @update:value="(value) => emit('update:payload', { ...payload, customerName: value.toString() })"
+      label="Nom du client"
+      placeholder="John Doe"
+      required
+    />
+    <FlapiInput
+      :value="props.payload.projectName"
+      @update:value="(value) => emit('update:payload', { ...payload, projectName: value.toString() })"
       label="Nom de l'application"
       placeholder="Enter the project name"
       required
     />
-    <FlapiInput v-model:value="payload.subdomain" label="Sous-domaine" placeholder="subdomain.example.com" required />
+    <FlapiInput
+      :value="props.payload.subdomain"
+      @update:value="(value) => emit('update:payload', { ...payload, subdomain: value.toString() })"
+      label="Sous-domaine"
+      placeholder="subdomain.example.com"
+      required
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-import type { Ref } from 'vue'
+import { defineProps } from 'vue'
+import type { PropType } from 'vue'
 import FlapiInput from '@/components/core/FlapiInput.vue'
+import type { CreateApplicationPayload } from '~/pages/setup/app/index.vue'
 /**
  * Type definitions for the CreateApplicationForm component props
- * @type {CreateApplicationPayload}
+ * @type {CreateApplicationFormProps}
  * @property {string} customerName - The name of the customer
  * @property {string} projectName - The name of the project
  * @property {string} subdomain - The subdomain of the project
  */
-export type CreateApplicationPayload = {
-  customerName: string
-  projectName: string
-  subdomain: string
+export type CreateApplicationFormProps = {
+  payload: CreateApplicationPayload
 }
 
-const payload: Ref<CreateApplicationPayload> = ref({
-  customerName: '',
-  projectName: '',
-  subdomain: '',
+const props: CreateApplicationFormProps = defineProps({
+  payload: {
+    type: Object as PropType<CreateApplicationPayload>,
+    default: {
+      customerName: '',
+      projectName: '',
+      subdomain: '',
+    },
+  },
 })
+
+/*EMIT*/
+const emit: (event: 'update:payload', value: CreateApplicationPayload) => void = defineEmits<{
+  (event: 'update:payload', value: CreateApplicationPayload): void
+}>()
 </script>
--- src-nuxt/pages/setup/app/index.vue
+++ src-nuxt/pages/setup/app/index.vue
@@ -4,8 +4,9 @@
       <p class="text-2xl font-semibold text-light-400">Photo de l'applications</p>
       <FlapiCardDownloadPicture picture="https://picsum.photos/136/136" />
       <p class="text-2xl font-semibold text-light-400">Personnalisation de l'application</p>
-      <FlapiFormCard>
-        <CreateApplicationForm />
+      <FlapiFormCard @confirm="createApp">
+        <CreateApplicationForm :payload="payload" @update:payload="payload = $event" />
+        payload: {{ payload }}
       </FlapiFormCard>
     </div>
   </div>
@@ -15,4 +16,41 @@
 import FlapiFormCard from '@/components/cards/FlapiFormCard.vue'
 import FlapiCardDownloadPicture from '@/components/cards/FlapiCardDownloadPicture.vue'
 import CreateApplicationForm from '@/components/forms/CreateApplicationForm.vue'
+
+/**
+ * Type definitions for the CreateApplicationForm component props
+ * @type {CreateApplicationPayload}
+ * @property {string} customerName - The name of the customer
+ * @property {string} projectName - The name of the project
+ * @property {string} subdomain - The subdomain of the project
+ */
+export type CreateApplicationPayload = {
+  customerName: string
+  projectName: string
+  subdomain: string
+}
+
+const payload: Ref<CreateApplicationPayload> = ref({
+  customerName: '',
+  projectName: '',
+  subdomain: '',
+})
+
+/**
+ * Create an application
+ * @returns {Promise<void>}
+ */
+const createApp: () => Promise<void> = async (): Promise<void> => {
+  // buttonLoading.value = true
+
+  try {
+    console.log('createApp', payload.value)
+    // Call application service
+    await new Promise((resolve: (value?: unknown) => void) => setTimeout(resolve, 200))
+  } catch (error: any) {
+    console.error(error)
+  } finally {
+    // buttonLoading.value = false
+  }
+}
 </script>
