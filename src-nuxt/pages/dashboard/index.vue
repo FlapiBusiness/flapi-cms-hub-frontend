244ef6b2ca3e82e0a8ddda3b22184bdead9a4cfe
Geoffrey Guillouët <geoffrey.guillouet@epitech.eu>
2025-03-14 12:06:22 +0100

    feat: add dashboard page with link

--- src-nuxt/pages/dashboard/index.vue
+++ src-nuxt/pages/dashboard/index.vue
@@ -1,28 +1,35 @@
 <template>
   <div class="flex h-full min-h-screen flex-col bg-gray-700 p-8">
     <FlapiCardWelcomeUser
-      :picture="fakeUser.picture"
-      :firstName="fakeUser.firstName"
-      :lastName="fakeUser.lastName"
-      :email="fakeUser.email"
+      :picture="'https://via.placeholder.com/120'"
+      :firstName="authenticatedUser?.firstname || ''"
+      :lastName="authenticatedUser?.lastname || ''"
+      :email="authenticatedUser?.email || ''"
     />
-    <div class="flex flex-1 items-center justify-center">
-      <FlapiCircularProgress :loading="incrementLoading" />
+    <div class="grid gap-3 p-4">
+      <FlapiLink v-for="(link, i) in links" :key="`link-${i}-${link.to}`" :link="link.to">
+        {{ link.text }}
+      </FlapiLink>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
 import FlapiCardWelcomeUser from '@/components/cards/FlapiCardWelcomeUser.vue'
 import { ref } from 'vue'
+import type { User } from '~~/src-core/api'
+
+const authenticatedUser: User | null = useAuthStore().authenticatedUser
 
 const incrementLoading: Ref<number> = ref(0)
 
-const fakeUser: { picture: string; firstName: string; lastName: string; email: string } = {
-  picture: 'https://via.placeholder.com/120',
-  firstName: 'John',
-  lastName: 'Doe',
-  email: 'john.doe@email.com',
-}
+const links: { text: string; to: string }[] = [
+  { text: 'Applications', to: '/dashboard/apps' },
+  { text: 'Dashboard Settings App', to: '/dashboard/settings/app' },
+  { text: 'Dashboard Settings User', to: '/dashboard/settings/user' },
+  { text: 'Dashboard Setup App', to: '/dashboard/setup/app' },
+  { text: 'Dashboard Users invite', to: '/dashboard/users/invite' },
+  { text: 'Dashboard Logs', to: '/dashboard/logs' },
+]
 
 // TODO: Remove this interval and use the real loading value
 setInterval(
